dec807a860271c61e1f7ba902ed4d7ccd5132bad
Clement-muriithi <muriithicngura@gmail.com>
2022-03-14 16:58:09 -0700

    taks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
 * main - entry point
 * my hello, printf
-* size 
+* size
 * Return: 0 after printng the function
 */
 int main(void)
@@ -12,5 +12,5 @@ printf("Size of a int: $d bytes(s) \n", sizeof(int));
 printf("Size of a long int: $d bytes(s) \n", sizeof(long int));
 printf("Size of a long long int: $d bytes(s) \n", sizeof(long long int));
 printf("Size of a float: $d bytes(s) \n", sizeof(float));
-return(0);
+return (0);
 }
