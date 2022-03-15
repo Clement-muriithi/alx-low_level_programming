1180fa1d95644778a0e1e7319d53bf2a8c27b4cb
Clement-muriithi <muriithicngura@gmail.com>
2022-03-15 09:50:41 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
 */
 void print_alphabet_x10(void)
 {
-int 1;
-char c;
-for (1 = 0; 1 < 10; ++i)
-{
-for (c = 'a'; c <= 'z'; c++;)
-{
-_putchar(c);
-}
--putchar('\n');
-}
+	int 1;
+	char c;
+	for (1 = 0; 1 < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++;)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
