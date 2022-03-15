4ebd7496024cfab232dfb0cfe5aa6ab335e59167
Clement-muriithi <muriithicngura@gmail.com>
2022-03-14 17:19:49 -0700

    task-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 /**
 * main - entry point
 *
-* Return: 0 after printng the function
+* Description: This is a great great function
+* Return: Always return 0 (success)
 */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
