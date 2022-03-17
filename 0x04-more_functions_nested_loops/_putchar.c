057825b6cb6aa3555cea38bbb943ff9ebe3f9c3c
Clement-muriithi <muriithicngura@gmail.com>
2022-03-17 12:05:51 -0700

    edits

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,12 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @num: The character to print
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(int num)
+int _putchar(char c)
 {
-	return (write(1, &num, 1));
+	return (write(1, &c, 1));
 }
